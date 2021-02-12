39fd2ff90144394b510bd71237c22122549d2941
MichaelShumate21 <54084339+MichaelShumate21@users.noreply.github.com>
2021-02-12 10:58:47 -0700

    Controller init
    Basic layout of controller to be improved

--- Mini - MASSIVE/src/Controller.java
+++ Mini - MASSIVE/src/Controller.java
@@ -1,2 +1,37 @@
 public class Controller {
+
+    int algoChoice;
+
+    public void Cont() {
+        //placeholder
+        setAlgoChoice(0);
+
+        //algorithm switch
+        switch (getAlgoChoice()) {
+            case 0:
+                //goto algo 0
+                break;
+
+            case 1:
+                //goto algo 1
+                break;
+            case 2:
+                //goto algo 2
+                break;
+            case 3:
+                //goto algo 3
+                break;
+            default:
+                //goto algo 0
+        }
+
+
+    }
+    public int getAlgoChoice () {
+        return algoChoice;
+    }
+
+    public void setAlgoChoice ( int algoChoice){
+        this.algoChoice = algoChoice;
+    }
 }
